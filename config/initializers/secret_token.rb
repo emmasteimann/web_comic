66f5aa4e61946a7e3f022dfd3bb3009a0e33295b
Emma Steimann <godwearspants@gmail.com>
2011-08-29 22:28:45 -0500

    Meow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyTime::Application.config.secret_token = 'efa084ce3d348b42d42ec40b2a0e8b53c90384e2db6f243210f1d31b8d5ba4c7e400b6f5c91732d34e3405125f05aaeae01fe99d6b0b2348a54f14f8c69a337e'
