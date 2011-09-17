ee8185c6b5b14fd14bbf1cde968e1979600c9e3a
Emma Steimann <godwearspants@gmail.com>
2011-09-17 16:47:49 -0500

    remove disqus config

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,6 @@ module PartyTime
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-    Disqus::defaults[:account] = 'emmasteimann'
-    Disqus::defaults[:developer] = true
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     # Enable the asset pipeline
