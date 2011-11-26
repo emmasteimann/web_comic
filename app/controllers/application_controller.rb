0c9c8e6f068444a7913c95007649b2c054570a31
Emma Steimann <godwearspants@gmail.com>
2011-11-26 00:35:17 -0600

    modified 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  rescue_from ActiveRecord::RecordNotFound do
-    render_404
-  end 
+  unless Rails.application.config.consider_all_requests_local
+  rescue_from Exception, :with => :render_404
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  rescue_from ActionController::UnknownController, :with => :render_404
+  rescue_from ActionController::UnknownAction, :with => :render_404
+  end
 
   def render_404
     respond_to do |type|
-      type.html { render :template => "#{RAILS_ROOT}/app/views/error/404", :layout => "application", :status => "404 Not Found" }
+      type.html { render :template => "error/404.html", :layout => "application", :status => "404 Not Found" }
       type.all  { render :nothing  => true, :status => "404 Not Found" }
     end
   end
